218d86229308fecd53c99cf2f2624eada32877e2
Shivpujan Yadav <shivpujan.userfacet@gmail.com>
2024-02-08 11:20:02 +0530

    small navbar edits

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -16,13 +16,13 @@ export default function NavBar() {
         <div className="text-muted">Last edit was made <b>8 minutes ago</b> by <b>someuser</b></div>
         <div className={`${style.shareBtn} rounded-pill`}>Share</div>
         <div className={`${style.presentBtn} rounded-pill`}>
-        <Image width={20} height={20} src="https://d1ehtu126lxvny.cloudfront.net/static/media/frontend/images/header/blue-right-arrow.svg" alt="play" />
+        <Image id="play" width={20} height={20} src="https://d1ehtu126lxvny.cloudfront.net/static/media/frontend/images/header/blue-right-arrow.svg" alt="play" />
         <div>Present</div>
         </div>
         <Image width={20} height={20} src="/ai-icon.svg" alt="export" />
         <Image width={20} height={20} src="https://d1ehtu126lxvny.cloudfront.net/static/media/frontend/images/assets/download.svg" alt="export" />
         <Image width={25} height={25} alt={"profile"} src="https://d1ehtu126lxvny.cloudfront.net/static/media/frontend/images/assets/comments-1.svg"/>
-        <Image width={35} height={35} alt={"profile"} src="https://d1ehtu126lxvny.cloudfront.net/user/media/default/user.svg"/>
+        <Image width={25} height={25} alt={"profile"} src="https://d1ehtu126lxvny.cloudfront.net/user/media/default/user.svg"/>
         
         
       </div>
